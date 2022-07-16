f696a826cf026b30a4a6fbd4c0490b084081b31b
micgarcia <micgarcia9185@gmail.com>
2022-07-16 12:08:32 -0700

    Finished Ship, started Gameboard factory

--- battleship.js
+++ battleship.js
@@ -3,5 +3,17 @@ export const Ship = function(length) {
   this.hit = function(pos) {
     this.positions[pos] = 'hit';
   }
+  this.isSunk = function() {
+    for(let i = 0; i < this.positions.length; i++) {
+      if (this.positions[i] !== 'hit') {
+        return false;
+      } else if (this.positions[i] === 'hit' && i === this.positions.length - 1) {
+        return true;
+      }
+    }
+  }
+}
 
+export const Gameboard = function() {
+  this.board = {/*board is an object that assigns each coord (eg 'A2') as a ship. Then it checks if an attack hits a key that has a ship value. Otherwise, it creates a new key with a value of miss};
 }
--- battleship.test.js
+++ battleship.test.js
@@ -1,4 +1,5 @@
-import { Ship } from './battleship.js';
+import { Ship, Gameboard } from './battleship.js';
+
 test('ship produces object with array of length', () => {
   const shipObject = new Ship(5);
   expect(shipObject.positions.length).toBe(5);
@@ -9,3 +10,15 @@ test('ship has hit function that marks position as hit', () => {
   shipObject.hit(3);
   expect(shipObject.positions[3]).toBe('hit');
 });
+
+test('ship has isSunk function that determines if all positions are hit', () => {
+  const shipObject = new Ship(2);
+  shipObject.hit(0);
+  shipObject.hit(1);
+  expect(shipObject.isSunk()).toBe(true);
+})
+
+
+test.todo('Gameboard has receiveAttack function that takes coords, sends hit to ship or miss'
+
+)
